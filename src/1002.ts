d9e9c95c448f5ed777c571e56e8d4072ef4c3017
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-15 11:31:37 -0300

    Feat: iniciado questao 1002

---
+++ src/1002.ts
@@ -0,0 +1,9 @@
+// A fórmula para calcular a área de uma circunferência é definida como A = π . R 2 . Considerando para este problema que π = 3,14159 :
+
+// Calcule a área usando a fórmula dada na descrição do problema.
+
+// Entrada
+// A entrada contém um valor de ponto flutuante (precisão dupla), que é a variável R .
+
+// Resultado
+// Apresente a mensagem "A=" seguida do valor da variável, como no exemplo abaixo, com quatro casas após a vírgula. Use todas as variáveis ​​de precisão dupla.
