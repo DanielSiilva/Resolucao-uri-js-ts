f7630015d394cf418ba8f57d5aef73f7711b9139
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-10-24 08:48:31 -0300

    Fix: se aplicado, correcoes exercicio 1013

---
+++ build/1013.js
@@ -0,0 +1,6 @@
+function User(nome, email) {
+    this.nome = nome;
+    this.email = email;
+    return `${nome} e ${email}`;
+}
+//# sourceMappingURL=1013.js.map
---
+++ src/1013.ts
@@ -0,0 +1,9 @@
+
+
+function User (nome:string, email:string){
+    this.nome = nome;
+    this.email = email;
+
+    return `${nome} e ${email}`
+}
+
