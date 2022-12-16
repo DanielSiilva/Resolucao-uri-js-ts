706eb8a888b25bad50ccad8566a25a582af08c0b
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-12-16 09:07:43 -0300

    Feat: finalizando o exercicio 1013

--- build/1013.js
+++ build/1013.js
@@ -1,6 +1,10 @@
-let [x, y] = ['Daniel', 'Marlene'];
-let obj = { x, y };
-const r = 3;
-const area = (Math.pow(r, 3));
-console.log(area);
+const numberA = Number(process.argv[2]);
+const numberB = Number(process.argv[3]);
+const numberC = Number(process.argv[4]);
+function NumberMaior(num1, num2, num3) {
+    let MaiorNum1Num2 = (num1 + num2 + Math.abs(num1 - num2)) / 2;
+    let Maior = (MaiorNum1Num2 + num3 + Math.abs(MaiorNum1Num2 - num3)) / 2;
+    return console.log(`${Maior} é o maior`);
+}
+NumberMaior(numberA, numberB, numberC);
 //# sourceMappingURL=1013.js.map
--- src/1013.ts
+++ src/1013.ts
@@ -1,11 +1,20 @@
-let [ x, y] = ['Daniel', 'Marlene']
+// Faça um programa que leia 3 valores inteiros e apresente o maior seguido da mensagem "eh o maior". Use a seguinte fórmula:
 
-let obj = {x, y}
 
-// console.log(obj)
+const numberA = Number(process.argv[2]);
+const numberB = Number(process.argv[3]);
+const numberC = Number(process.argv[4]);
 
-const r:number = 3
 
-const area =(r ** 3)
 
-console.log(area)
+function NumberMaior (num1:number,num2:number,num3:number){
+
+    let MaiorNum1Num2 = (num1+num2 + Math.abs(num1-num2))/2
+
+    let Maior =(MaiorNum1Num2 + num3 + Math.abs(MaiorNum1Num2-num3)) /2
+
+    return console.log(`${Maior} é o maior`)
+}
+
+NumberMaior(numberA,numberB,numberC)
+
